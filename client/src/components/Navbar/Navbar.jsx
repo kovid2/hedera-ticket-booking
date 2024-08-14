00ae583ddb4d424b4dcb870b8d7035ee612d1da9
chocoboash <anleal2004@gmail.com>
2024-08-14 02:52:05 -0400

    feat: more navbar styling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import './Navbar.scss';
 import { useContext } from 'react';
 import { GlobalAppContext } from '../../contexts/GlobalAppContext';
@@ -7,15 +6,18 @@ import { fetchUser } from '../../network/api';
 
 import ticketByte from '../../assets/ticketByte.svg';
 import metamask from '../../assets/metaMaskLogo.png';
+import shoppingCart from '../../assets/shoppingCart.svg';
+import user from '../../assets/user.svg';
 
 export default function NavBar() {
   // use the GlobalAppContext to keep track of the metamask account connection
   const { metamaskAccountAddress, setMetamaskAccountAddress } = useContext(GlobalAppContext);
 
   const retrieveWalletAddress = async () => {
-	console.log("retrieving wallet address");
+    console.log("retrieving wallet address");
     const addresses = await connectToMetamask();
-	console.log(addresses);
+    console.log(addresses);
+
     if (addresses) {
       // grab the first wallet address
       setMetamaskAccountAddress(addresses[0]);
@@ -26,31 +28,40 @@ export default function NavBar() {
   }
 
   return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <img src={ticketByte} alt="ticketByte" />
 
-        <div className="navbar"> 
-
-          <div className="navbar-container">
-            
-            <img src={ticketByte} alt="ticketByte" />
-
-            <div className="navbar-button-container">
-
-              <div className="navbar-button">
-              <button
-          
-          onClick={retrieveWalletAddress}
-        >
-          {metamaskAccountAddress === "" ?
-            "Connect to MetaMask" :
-            `Connected to: ${metamaskAccountAddress.substring(0, 8)}...`}
-                  <img src={metamask} alt="metamask" />
-                </button> 
-
-              </div>
-            </div>
-          
+        <div className="navbar-links">
+          <a href="/Events">Events</a>
+          <a href="/Tickets">My Tickets</a>
         </div>
+
+        <div className="navbar-button-container">
+          <div className="navbar-button">
+            <button onClick={retrieveWalletAddress}>
+              {metamaskAccountAddress === "" ?
+                " " :
+                `Connected to: ${metamaskAccountAddress.substring(0, 8)}...`
+              }
+              <img src={metamask} alt="metamask" />
+            </button>
           </div>
-        
-  )
+
+          <div className="navbar-button">
+            <button>
+              <img src={user} alt="user" />
+            </button>
+          </div>
+
+          <div className="navbar-button">
+            <button>
+              <img src={shoppingCart} alt="shoppingCart" />
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 }
